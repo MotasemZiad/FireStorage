f9e7a91b93f2940fcc5c40231374432517bd3e24
Motasem Z. AbuNema <52855540+MotasemZiad@users.noreply.github.com>
2021-04-09 20:14:17 +0300

    finish work

--- app/src/main/java/com/ziad/motasem/firestorage/MainActivity.kt
+++ app/src/main/java/com/ziad/motasem/firestorage/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.ziad.motasem.firestorage
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.ziad.motasem.firestorage.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private val TAG = "mzn"
+    private val pickImageRequest = 6000
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var filePath: Uri
+    private lateinit var storageReference: StorageReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnPick.setOnClickListener(this)
+        binding.btnUpload.setOnClickListener(this)
+        storageReference = FirebaseStorage.getInstance().reference
+
+
+    }
+
+    override fun onClick(view: View?) {
+        if (view == binding.btnPick) {
+            pickImage()
+        } else if (view == binding.btnUpload) {
+            uploadImage()
+        }
+    }
+
+    private fun pickImage() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Select an Image"), pickImageRequest)
+    }
+
+    private fun uploadImage() {
+        val imgReference = storageReference.child("images")
+        val childReference = imgReference.child(System.currentTimeMillis().toString())
+        imgReference.putFile(filePath).addOnSuccessListener {
+            Log.e(TAG, "Success")
+        }.addOnFailureListener {
+            Log.e(TAG, "Fail ${it.message.toString()}")
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == pickImageRequest && resultCode == RESULT_OK && data != null && data.data != null) {
+            filePath = data.data!!
+            binding.imageStorage.setImageURI(filePath)
+        }
     }
 }
